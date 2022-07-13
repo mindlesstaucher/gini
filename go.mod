8449814c9707e000b2c1757c03923afb012560c1
mbecker <marco.becker@pfreundt.de>
2022-07-13 17:13:39 +0200

    MQTT

--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module github.com/mindlesstaucher/gini
 go 1.18
 
 require (
+	github.com/eclipse/paho.mqtt.golang v1.4.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/goccy/go-json v0.9.8 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -21,6 +23,7 @@ require (
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/net v0.0.0-20220708220712-1185a9018129 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220708085239-5a0f0661e09d // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"os"
 
@@ -10,9 +11,67 @@ import (
 	"github.com/mindlesstaucher/gini/api/v1/material"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+
+	"strconv"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
-func main() {
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected")
+
+	publish(client)
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("Connect lost: %v", err)
+}
+
+func publish(client mqtt.Client) {
+	num := 10
+	for i := 0; i < num; i++ {
+		text := fmt.Sprintf("Message %d", i)
+		token := client.Publish("/help/2", 0, false, text)
+		token.Wait()
+		time.Sleep(time.Second)
+	}
+}
+
+func sub(client mqtt.Client, serviceInstance int) {
+	topic := "/help/1"
+	token := client.Subscribe(topic, 1, nil)
+	token.Wait()
+	fmt.Printf("Subscribed to topic %s", topic)
+}
+
+func SetupMqtt(serviceInstance int) mqtt.Client {
+	var broker = "10.0.4.74"
+	var port = 1883
+
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
+	opts.SetClientID("go_mqtt_client")
+	opts.SetUsername(fmt.Sprintf("pfreundt-service-%d", serviceInstance))
+	opts.SetPassword("Pfreundt1979!")
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	sub(client, serviceInstance)
+
+	return client
+}
+
+func SetupDb(serviceInstance int) *gorm.DB {
 
 	var db *gorm.DB
 	var err error
@@ -25,7 +84,17 @@ func main() {
 		}
 	}
 
-	db, err = gorm.Open(sqlite.Open("db/database.db"), &gorm.Config{})
+	path = fmt.Sprintf("db/%d", serviceInstance)
+	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+		err := os.Mkdir(path, os.ModePerm)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	path = fmt.Sprintf("db/%d/database.db", serviceInstance)
+
+	db, err = gorm.Open(sqlite.Open(path), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
@@ -33,6 +102,10 @@ func main() {
 	db.AutoMigrate(&material.MaterialModel{})
 	db.AutoMigrate(&customer.CustomerModel{})
 
+	return db
+}
+
+func RunWebApi(db *gorm.DB, serviceInstance int) {
 	r := gin.Default()
 
 	r.GET("/api/v1/customer", customer.GetCustomer(db))
@@ -41,5 +114,34 @@ func main() {
 
 	r.GET("/api/v1/material", material.MaterialGet(db))
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	addr := fmt.Sprintf(":%d", 8080+serviceInstance)
+	r.Run(addr)
+}
+
+func GetServiceInstance() int {
+	var serviceInstance int = 0
+	programName := os.Args[0]
+	fmt.Println(programName)
+	argLength := len(os.Args[1:])
+	fmt.Printf("Arg length is %d\n", argLength)
+	if argLength >= 1 {
+		serviceInstance, _ = strconv.Atoi(os.Args[1])
+	}
+	fmt.Printf("serviceInstance is %d", serviceInstance)
+
+	return serviceInstance
+}
+
+func main() {
+
+	var db *gorm.DB
+
+	serviceInstance := GetServiceInstance()
+
+	SetupMqtt(serviceInstance)
+
+	db = SetupDb(serviceInstance)
+
+	RunWebApi(db, serviceInstance)
+
 }
